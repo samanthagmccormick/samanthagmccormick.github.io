af3afc59e21c6e3a806e54b407ebdb2973c399c1
fa7b53c24b72c491e28d4a4aef4c0169d3f1513e <samanthagmccormick@gmail.com>
2016-01-31 18:28:12 -0700

    res.send

--- json-test/server.js
+++ json-test/server.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB: Connect to local database named rest_test
-mongoose.connect('mongodb://localhost/rest_test');
+mongoose.connect('mongodb://samanthagmccormick.github.io/rest_test');
 
 // Express
 var app = express();
@@ -16,12 +16,12 @@ app.use(bodyParser.json());
 
 // Routes
 app.get('/', function(req, res) {
-  res.send("index.html");
+  res.send("hi");
 });
 
-app.post('handle', function(req, res) {
-  var body = request.body;
-});
+// app.post('handle', function(req, res) {
+//   var body = request.body;
+// });
 
 // Anything that comes through with /api/, direct it to this location
 app.use('/api', require('./routes/api'));
