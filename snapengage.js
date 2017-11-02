3805968630ecb063504f79841a0a9cdbd2e62908
f24e3edbeb91f8e3c850874b6806432241f1820a <samanthagmccormick@users.noreply.github.com>
2017-11-02 10:49:49 -0600

    Proof that iframe needs its own window.onerror otherwise it's null

--- snapengage.js
+++ snapengage.js
@@ -5,28 +5,12 @@
 
 console.log('snapengage.js loaded');
 
-
 window.onerror = function (msg, url, lineNo, columnNo, error) {
-  console.log("window onerror fired");
-  var string = msg.toLowerCase();
-  var substring = "script error";
-  alert("custom window.onerror has fired");
-  if (string.indexOf(substring) > -1) {
-    alert('Script Error: ' + msg);
-  } else {
-    var message = [
-      'Message: ' + msg,
-      'URL: ' + url,
-      'Line: ' + lineNo,
-      'Column: ' + columnNo,
-      'Error object: ' + error.stack
-    ].join(' - ');
-
-    alert(message);
-  }
-
-  return false;
+  alert("snapengage.js window.onerror has fired");
 };
+
+console.log("SNAPENGAGE.js window.onerror: " + window.onerror);
+
 
 // This DOES bubble up to call window.onerror();
 // somethingggg();
