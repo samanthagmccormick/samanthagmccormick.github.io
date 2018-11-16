fc3a65597ba909baf26f902ac42e5fbb1387e05b
f24e3edbeb91f8e3c850874b6806432241f1820a <samanthagmccormick@users.noreply.github.com>
2018-11-16 15:55:59 -0700

    Commented out scripts are loading for some reason - removing

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 // load header and footer includes
 $(function() {
-  console.log('loading header and footer');
   $('#header').load('./inc/header.html');
   $('#footer').load('./inc/footer.html');
   $('#menu').load('./inc/menu.html');
 });
-
-
