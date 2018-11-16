cf364ff9c9d514d036edbbc0ab23d66f20ffd5db
f24e3edbeb91f8e3c850874b6806432241f1820a <samanthagmccormick@users.noreply.github.com>
2018-11-16 12:51:46 -0700

    Removing previous site, putting Jackson's test page up

---
+++ json.js
@@ -0,0 +1,48 @@
+// using localhost orgId and widget
+// var localhostUrl = 'http://127.0.0.1:8888/api/v2/5629499534213120/logs?start=2018-01-01&end=2018-01-04';
+
+// $.ajax({
+//   // beforeSend: function(xhrObj) {
+//   //   xhrObj.setRequestHeader("Content-Type", "application/json");
+//   //   xhrObj.setRequestHeader("Accept", "application/json");
+//   //   xhrObj.setRequestHeader('Access-Control-Allow-Origin', '*');
+//   //   xhrObj.setRequestHeader("x-api-key", "3d73692b1b019ea1fc18c0c5c703093d");
+//   // },
+//   headers: {
+//     "x-api-key": "3d73692b1b019ea1fc18c0c5c703093d"
+//   },
+//   dataType: "json",
+//   contentType: "application/json; charset=utf-8",
+//   url: localhostUrl,
+//   method: "GET",
+//   crossDomain: true,
+//   success: function(data) {
+//     console.log(data);
+//     var count = 0;
+//     for (i in data.cases) {
+//       count++;
+//     }
+//     console.log("count of cases: " + count);
+//   },
+//   error: function(jqXHR, textStatus, errorThrown) {
+//     console.log("error: " + errorThrown);
+//   }
+// });
+
+// $.getJSON("jsonobject.json", function(data, textStatus, jqXHR) {
+//   var count = 0;
+//   $.each(data.cases, function(i) {
+
+//     count++;
+//     // Print dates
+//     console.log(moment(data.cases[i].created_at_date)._d);
+
+//     // Print users
+//     console.log(data.cases[i].requested_by);
+
+//     // Print case id
+//     console.log(data.cases[i].id);
+
+//   });
+//   console.log("count: " + count);
+// });
---
+++ main.js
@@ -0,0 +1,9 @@
+// load header and footer includes
+$(function() {
+  console.log('loading header and footer');
+  $('#header').load('./inc/header.html');
+  $('#footer').load('./inc/footer.html');
+  $('#menu').load('./inc/menu.html');
+});
+
+
